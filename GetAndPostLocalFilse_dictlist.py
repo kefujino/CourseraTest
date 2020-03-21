d48747ccd77a642606e1323cc8319b6474010cd3
kefujino <k.fujino0801@gmail.com>
2020-03-21 12:28:37 +0900

    Createted
    https://googlecoursera.qwiklabs.com/focuses/52112

---
+++ GetAndPostLocalFilse_dictlist.py
@@ -0,0 +1,28 @@
+! /usr/bin/env python3
+import os
+import requests
+from collections import defaultdict
+
+# Please replace
+url = 'http://<corpweb-external-IP>/feedback'
+
+# 辞書の初期化
+fb_dict = defaultdict(list)
+
+# get feedback(fb) list
+filepath = '/data/feedback'
+files  = os.listdir(filepath)
+
+# traverse each files
+for file in files:
+  f = open('/data/feedback/'+file, "r")
+  line = f.readlines()
+  f.close()
+
+  fb_dict['title'].append(line[0][:-1])
+  fb_dict['name'].append(line[1][:-1])
+  fb_dict['date'].append(line[2][:-1])
+  fb_dict['feedback'].append(line[3][:-1])
+
+  response = requests.post(url, json=fb_dict)
+  print(response.status_code)
